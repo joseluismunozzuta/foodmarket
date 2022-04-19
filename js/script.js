9574fd76f8ab1f8162b51dd24a593206051d0493
joseluismunozzuta <joseluismunozzuta@gmail.com>
2022-04-19 18:38:05 -0500

    Avance final

--- js/script.js
+++ js/script.js
@@ -7,41 +7,153 @@ class Producto {
 }
 
 let totalproductos = 0;
+const productosLista = [];
 
+/***********SE LEE ARCHIVO JSON**********/
 fetch('/dataproductos.json')
     .then((res) => res.json())
     .then((data) => {
         data.forEach((producto) => {
             totalproductos++;
-            listarProductos(producto);
+            listarProductos(new Producto(producto));
+            productosLista.push(new Producto(producto));
         })
         document.getElementById("totaldeproductos").innerText = totalproductos;
-    })
+    });
+/****************************************/
 
-const prod1 = { id: 1, nombre: "Agua Cielo", precio: 1 };
-const prod2 = { id: 2, nombre: "Agua San Luis", precio: 2 };
-const prod3 = { id: 3, nombre: "Galleta Soda", precio: 1 };
-const prod4 = { id: 4, nombre: "Papitas Lays", precio: 2 };
-const prod5 = { id: 5, nombre: "Chocolate Sublime", precio: 3 };
-const arregloProductos = [prod1, prod2, prod3, prod4, prod5];
+function listarProductos(producto) {
+    let nombreprod = producto.nombre;
+    let precioprod = producto.precio;
+    let idprod = producto.id;
 
-const [a, b] = arregloProductos;
-console.log("Este es el nombre del producto 1: " + a.nombre);
-console.log("Este es el nombre del producto 2: " + b.nombre);
+    console.log("El nombre del producto es " + nombreprod);
+    let target = document.getElementById("listaproductoscontainer");
 
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
-
-//********************LOCAL STORAGE*************************//
-guardarLocal("listadeProductos", JSON.stringify(arregloProductos));
-
-const prodAlmacenados = JSON.parse(localStorage.getItem("listadeProductos"));
-const productos = [];
-
-//Iteramos los objetos almacenados para transformarlos a productos
-for (const objeto of prodAlmacenados) {
-    productos.push(new Producto(objeto));
+    target.innerHTML += "<div class='filadivisora separate' id='div" + idprod + "'><input readonly class='form-control nombreprod' value='" + idprod + ". | " + producto.nombre + "  | Precio: S/. " + precioprod + "'id='prod" + idprod + "' name='nombreproducto'><img class='iconomas' src='/images/iconomas.jpg' id='iconomasssss" + idprod + "' onclick='agregarProd(id)' alt=''><img class='iconomenos' src='/images/iconomenos.jpg' id='iconomenosss" + idprod + "' onclick='disminuirProd(id)' alt=''></div>";
 }
-//**********************************************************//
+
+function buscarProd() {
+    let texto = document.getElementById("productoBuscado").value;
+    if (texto.localeCompare('') === 0) {
+
+        for (var x = 1; x <= totalproductos; x++) {
+
+            document.getElementById("div" + x).style.display = "flex";
+        }
+    }
+    for (var i = 1; i <= totalproductos; i++) {
+        let nombreprod = document.getElementById("prod" + i).value;
+        if (!(nombreprod.toLowerCase().includes(texto.toLowerCase()))) {
+            document.getElementById("div" + i).style.display = "none";
+        } else if ((nombreprod.toLowerCase().includes(texto.toLowerCase()))) {
+            document.getElementById("div" + i).style.display = "flex";
+        }
+    }
+}
+
+function agregarProd(id) {
+
+    let idproducto = id.substring(12, 13); //Tenemos que solo obtener el id numerico, sin la palabra icono mas o menos.
+
+    const prodElegido = productosLista[idproducto - 1];
+    console.log(prodElegido);
+    let nombreprod = prodElegido.nombre;
+    let precioprod = prodElegido.precio;
+    let idprod = prodElegido.id;
+
+    console.log("El producto elegido es " + nombreprod);
+    let target = document.getElementById("productocontainer");
+    let varFlag = document.getElementById("cantidadprod" + idprod);
+    if (varFlag) {
+        console.log("La cantidad de este producto SI tiene un valor");
+        let cantidadFlag = document.getElementById("cantidadprod" + idprod).innerText;
+        cantidadFlag = cantidadFlag.valueOf();
+        cantidadFlag++;
+        document.getElementById("cantidadprod" + idprod).innerText = cantidadFlag;
+    } else {
+        console.log("No existia este producto en el carrito. Se agrega div.");
+        target.innerHTML += "<div style='border: black 2px solid; padding: 5px' class='separate filacont' id='prodagregado" + idprod + "'><span style='width: 25%; text-align: center'><b>" + nombreprod + "</b></span><span>Costo: </span><span style='width: 8%; text-align: center'>S/. " + precioprod + "</span><span>Cantidad: </span><span style='width: 5%; text-align: center; font-weight: bold'   id='cantidadprod" + idprod + "'>1</span></div>";
+        document.getElementById("cantidadprod" + idprod).innerText = "1";
+    }
+
+    let string2 = "";
+    for (let j = 1; j <= totalproductos; j++) {
+        /*comenzamos con idproductos*/
+        let objetivo = document.getElementById("prodagregado" + j);
+        if (objetivo) {
+            console.log("Se tiene en cuenta el producto con orden " + j);
+            let cantidad = document.getElementById("cantidadprod" + j).innerText;
+            string2 = string2.concat(j + ',' + cantidad.valueOf());
+        } else {
+            console.log("No existe el producto con orden " + j);
+        }
+    }
+    document.getElementById("pedidocantidad").value = string2;
+    let clavefinal = document.getElementById("pedidocantidad").value;
+    if (clavefinal === "") {
+        console.log("No existen productos para agregar al pedido.");
+        document.getElementById("botonElegir").style.display = "none";
+    } else {
+        document.getElementById("botonElegir").style.display = "inline-block";
+        console.log("La clave es " + clavefinal);
+    }
+}
+
+function disminuirProd(id) {
+    let idproducto = id.substring(12, 13); //Tenemos que solo obtener el id numerico, sin la palabra icono mas o menos.
+
+    const prodElegido = productosLista[idproducto - 1];
+    console.log(prodElegido);
+    let nombreprod = prodElegido.nombre;
+    let precioprod = prodElegido.precio;
+    let idprod = prodElegido.id;
+    let varFlag = document.getElementById("cantidadprod" + idprod);
+    console.log("La cantidad de este producto es " + varFlag);
+    if (varFlag) {
+        console.log("La cantidad de este producto SI tiene un valor");
+        let cantidadFlag = document.getElementById("cantidadprod" + idprod).innerText;
+        cantidadFlag = cantidadFlag.valueOf();
+        if (cantidadFlag == 1) {
+            console.log("La cantidad de este producto era 1, se procede a remover el div.");
+            let target = document.getElementById("prodagregado" + idprod);
+            target.remove();
+            console.log("Se removio el div del producto.");
+        } else {
+            cantidadFlag--;
+            console.log("La cantidad de este producto disminuye a " + cantidadFlag);
+            document.getElementById("cantidadprod" + idprod).innerText = cantidadFlag;
+        }
+        actulizarClave();
+    } else {
+        console.log("El producto que se trata de disminuir no existe.");
+    }
+}
+
+function actulizarClave() {
+    
+    let string2 = "";
+    for (let j = 1; j <= totalproductos + 1; j++) {
+        /*comenzamos con idproductos*/
+        let objetivo = document.getElementById("prodagregado" + j);
+        if (objetivo) {
+            console.log("Se tiene en cuenta el producto con orden " + j);
+            let cantidad = document.getElementById("cantidadprod" + j).innerText;
+            string2 = string2.concat(j + ',' + cantidad.valueOf());
+        } else {
+            console.log("No existe el producto con orden " + j);
+        }
+    }
+    document.getElementById("pedidocantidad").value = string2;
+    let clavefinal = document.getElementById("pedidocantidad").value;
+    if (clavefinal === "") {
+        console.log("No existen productos para agregar al pedido.");
+        document.getElementById("botonElegir").style.display = "none";
+    } else {
+        document.getElementById("botonElegir").style.display = "inline-block";
+        console.log("La clave es " + clavefinal);
+    }
+}
 
 function probandoData(producto) {
     console.log(producto.nombre);
@@ -50,7 +162,25 @@ function probandoData(producto) {
 }
 
 let boton = document.getElementById("botonElegir");
-boton.addEventListener("click", elegirProductos);
+boton.addEventListener("click", () => {
+    Swal.fire({
+        title: '¿Quieres realizar tu compra?',
+        text: 'Hiciste click en el botón "Realizar compra"',
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, seguro',
+        cancelButtonText: 'No, no quiero'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            borrarProductos();
+            Swal.fire({
+                title: 'Compra realizada!',
+                icon: 'success',
+                text: 'Gracias por comprar con nosotros'
+            })
+        }
+    })
+});
 
 let boton2 = document.getElementById("botonBorrar");
 boton2.addEventListener("click", () => {
@@ -73,61 +203,7 @@ boton2.addEventListener("click", () => {
     })
 });
 
-function listarProductos(producto) {
-    let nombreprod = producto.nombre;
-    let precioprod = producto.precio;
-    let idprod = producto.id;
-
-    console.log("El nombre del producto es " + nombreprod);
-    let target = document.getElementById("listaproductoscontainer");
-
-    target.innerHTML += "<div class='filadivisora separate' id='div" + idprod + "'><input readonly class='form-control nombreprod' value='" + producto.nombre + "'id='prod" + idprod + "' name='nombreproducto'><img class='iconomas' src='/images/iconomas.jpg' id='iconomasssss" + idprod + "' onclick='agregarProd(id)' alt=''><img class='iconomenos' src='/images/iconomenos.jpg' id='iconomenosss" + idprod + "' onclick='disminuirProd(id)' alt=''></div>";
-}
-
-function buscarProd() {
-    let texto = document.getElementById("productoBuscado").value;
-    if (texto.localeCompare('') === 0) {
-
-        for (var x = 1; x <= totalproductos; x++) {
-
-            document.getElementById("div" + x).style.display = "flex";
-        }
-    }
-    for (var i = 1; i <= totalproductos; i++) {
-        let nombreprod = document.getElementById("prod" + i).value;
-        if (!(nombreprod.toLowerCase().includes(texto.toLowerCase()))) {
-            document.getElementById("div" + i).style.display = "none";
-        } else if ((nombreprod.toLowerCase().includes(texto.toLowerCase()))) {
-            document.getElementById("div" + i).style.display = "flex";
-        }
-    }
-}
-
-function elegirProductos() {
-
-    let prodElegidos = prompt("Ingrese el numero de los productos deseados separados por una coma. Si desea los productos 1 y 2, ingrese: 1,2");
-
-    (!(prodElegidos == "" || prodElegidos == null)) ? printConsoleLogProducts(prodElegidos) : alert("Debe ingresar al menos un producto");
-}
-
-function printConsoleLogProducts(productosElegidos) {
-    var arrayIdsProd = productosElegidos.split(",");
-    for (let i = 0; i < arrayIdsProd.length; i++) {
-        var temp = arrayIdsProd[i];
-        const nom = { ...productos[temp - 1] }
-        console.log("El cliente eligio el sgte producto: ");
-        console.log(nom);
-        agregarProductos(arrayIdsProd[i]);
-    }
-}
-
 function borrarProductos() {
-    document.getElementById("seccionEleccion").innerHTML = '';
+    document.getElementById("productocontainer").innerHTML = '';
 }
-
-function agregarProductos(idElegidos) {
-    document.getElementById("seccionEleccion").innerHTML +=
-        "<div><h4>Se eligio 1 " + productos[idElegidos - 1]?.nombre + " con un precio de " + productos[idElegidos - 1]?.precio + " soles</h4>";
-}
-
 
